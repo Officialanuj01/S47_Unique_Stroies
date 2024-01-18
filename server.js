708ed1360357e289d67c445b5513d66b58915aea
004Anuj <anuj.sahu@kalvium.community>
2024-01-18 11:51:46 +0530

    server.js page added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+
+if (process.env.NODE_ENV !== "test") {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
